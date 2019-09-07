8bc0c8eff5ac5b4aa49b0f653d18fa98023f9c33
Fátima Correa <54726730+FatimaCo@users.noreply.github.com>
2019-09-07 15:41:32 -0300

    Add files via upload
    Opcional

---
+++ ecommerce-master/js/products.js
@@ -0,0 +1,40 @@
+var currentProductsArray=[];
+
+function showProductsList(currentProductsArray){
+    
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let products = currentProductsArray[i];
+        htmlContentToAppend += `
+        <a href="products.html" class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ products.name +`</h4>
+                        <small class="text-muted">`+ products.currency + ` ` + products.cost + ` </small>
+                    </div>
+                    <p class="mb-1">` + products.description + `</p>
+                </div>
+            </div>
+        </a>
+        `
+    }
+
+    document.getElementById("products-list-container").innerHTML = htmlContentToAppend;
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            currentProductsArray = resultObj.data;
+            showProductsList(currentProductsArray);
+        }
+    })
+});
---
+++ ecommerce-master/js/signin.js
@@ -0,0 +1,6 @@
+var página = "sign-in.html"
+
+function redireccionar(){
+    location.href = página
+}
+setTimeout (redireccionar(), 500);
