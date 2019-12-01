c4b4e6f60d406eca4803fbf7a5a4997284c04874
Fátima Correa <54726730+FatimaCo@users.noreply.github.com>
2019-12-01 11:26:39 -0300

    Update init.js

--- ecommerce-master/js/init.js
+++ ecommerce-master/js/init.js
@@ -7,35 +7,97 @@ const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
 
-var showSpinner = function(){
-  document.getElementById("spinner-wrapper").style.display = "block";
+function saveData() {
+    sessionStorage.setItem('user', document.getElementById("inputName").value);
 }
 
-var hideSpinner = function(){
-  document.getElementById("spinner-wrapper").style.display = "none";
+    var user = sessionStorage.getItem("user");
+
+function showData() {
+    if (user != null) {
+        document.getElementById("userName").innerHTML = user;
+    } else {
+        document.getElementById("userName").innerHTML = "Invitado";
+    }
 }
 
-var getJSONData = function(url){
+function showMessage() {
+    if (user != null) {
+        document.getElementById("message").innerHTML = "Cerrar Sesión";
+    } else {
+        document.getElementById("message").innerHTML = "Iniciar Sesión";
+    }
+}
+
+var page = "sign-in.html"
+
+function redirect() {
+    location.href = page
+}
+
+function logOut() {
+    sessionStorage.removeItem("user");
+    redirect();
+}
+
+function onSignIn(googleUser) 
+{
+      var profile = googleUser.getBasicProfile(); 
+      {
+        sessionStorage.setItem('usuario', profile.getName());
+      }
+      {
+        window.location.assign("https://FatimaCo.github.io/ecommerce-master/index.html");
+      }
+}
+ 
+function signOut() 
+{
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () 
+    {
+    document.getElementById("userName").innerHTML = 'Invitado';
+    window.location.assign("https://FatimaCo.github.io/ecommerce-master/sign-in.html");
+    });
+}
+
+function onLoad() 
+{
+      gapi.load('auth2', function() 
+      {
+        gapi.auth2.init();
+      });
+}
+
+var showSpinner = function() {
+    document.getElementById("spinner-wrapper").style.display = "block";
+}
+
+var hideSpinner = function() {
+    document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+var getJSONData = function(url) {
     var result = {};
     showSpinner();
     return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
-    });
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function(response) {
+            result.status = 'ok';
+            result.data = response;
+            hideSpinner();
+            return result;
+        })
+        .catch(function(error) {
+            result.status = 'error';
+            result.data = error;
+            hideSpinner();
+            return result;
+        });
 }
